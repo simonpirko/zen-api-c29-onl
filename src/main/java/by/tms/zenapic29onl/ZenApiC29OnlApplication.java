932fa9a19c6ed3195ebc482d4dec6c924544e988
Simon Pirko <sipi7gm@gmail.com>
2024-10-09 09:59:05 +0300

    Initial commit

---
+++ src/main/java/by/tms/zenapic29onl/ZenApiC29OnlApplication.java
@@ -0,0 +1,13 @@
+package by.tms.zenapic29onl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenApiC29OnlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZenApiC29OnlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/tms/zenapic29onl/ZenApiC29OnlApplicationTests.java
@@ -0,0 +1,13 @@
+package by.tms.zenapic29onl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenApiC29OnlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

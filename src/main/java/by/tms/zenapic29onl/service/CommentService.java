60f4610546f2b1793634593ea6ce0508fe79e02f
Yakubchyk <ivan.yakubchyk@gmail.com>
2024-10-14 18:18:35 +0200

    4.0 CommentService +

--- src/main/java/by/tms/zenapic29onl/service/CommentService.java
+++ src/main/java/by/tms/zenapic29onl/service/CommentService.java
@@ -1,34 +1,44 @@
 package by.tms.zenapic29onl.service;
 
 import by.tms.zenapic29onl.entity.Comment;
+import by.tms.zenapic29onl.exception.NotFoundException;
+import by.tms.zenapic29onl.repository.CommentRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentService implements CrudOperation<Comment> {
+    private final CommentRepository commentRepository;
 
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
 
     @Override
     public Comment save(Comment comment) {
-        return null;
+        return commentRepository.save(comment);
     }
 
     @Override
     public void delete(Comment comment) {
-
+        commentRepository.delete(comment);
     }
 
     @Override
     public void delete(Long id) {
-
+        Comment comment = findById(id);
+        commentRepository.delete(comment);
     }
 
     @Override
     public Comment update(Comment comment) {
-        return null;
+        Comment existingComment = findById(comment.getId());
+        existingComment.setDescription(comment.getDescription());
+        return commentRepository.save(existingComment);
     }
 
     @Override
     public Comment findById(Long id) {
-        return null;
+        return commentRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Comment not Found"));
     }
 }

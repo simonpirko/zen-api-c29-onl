8a321eb9ad8ae72313f4d1cd1a951428457b0350
Yakubchyk <ivan.yakubchyk@gmail.com>
2024-10-14 17:49:19 +0200

    3.1 PostService impl CrudOperation

--- src/main/java/by/tms/zenapic29onl/service/PostService.java
+++ src/main/java/by/tms/zenapic29onl/service/PostService.java
@@ -1,13 +1,50 @@
 package by.tms.zenapic29onl.service;
 
 import by.tms.zenapic29onl.entity.Post;
-import java.util.List;
+import by.tms.zenapic29onl.exception.NotFoundException;
+import by.tms.zenapic29onl.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 
-public interface PostService {
-    Post createPost(Post post);
-    Post updatePost(Long id, Post post);
-    Post getPostById(Long id);
-    List<Post> getAllPosts();
-    void deletePost(Long id);
+@Service
+public class PostService implements CrudOperation<Post> {
+
+    private final PostRepository postRepository;
+
+    @Autowired
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public Post save(Post post) {
+        return postRepository.save(post);
+    }
+
+    @Override
+    public void delete(Post post) {
+        postRepository.delete(post);
+
+    }
+
+    @Override
+    public void delete(Long id) {
+        postRepository.deleteById(id);
+
+    }
+
+    @Override
+    public Post update(Post post) {
+        Post existingPost = postRepository.findById(post.getId()).orElseThrow(() -> new NotFoundException("Post not Found"));
+        existingPost.setTitle(post.getTitle());
+        existingPost.setContents(post.getContents());
+        return postRepository.save(existingPost);
+
+    }
+
+    @Override
+    public Post findById(Long id) {
+        return postRepository.findById(id).orElseThrow(() -> new NotFoundException("Post not Found"));
+    }
 }

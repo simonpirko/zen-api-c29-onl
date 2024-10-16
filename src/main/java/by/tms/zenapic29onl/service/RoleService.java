8b9de9285fdb3ac7102b699523318d4511707194
Yakubchyk <ivan.yakubchyk@gmail.com>
2024-10-16 15:41:59 +0200

    13. Исправления PostService, RoleService, SubscriberService, UserService

--- src/main/java/by/tms/zenapic29onl/service/RoleService.java
+++ src/main/java/by/tms/zenapic29onl/service/RoleService.java
@@ -3,11 +3,10 @@ package by.tms.zenapic29onl.service;
 import by.tms.zenapic29onl.entity.Role;
 import by.tms.zenapic29onl.exception.NotFoundException;
 import by.tms.zenapic29onl.repository.RoleRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class RoleService implements CrudOperation<Role> {
+public class RoleService {
 
     private RoleRepository roleRepository;
 
@@ -15,30 +14,25 @@ public class RoleService implements CrudOperation<Role> {
         this.roleRepository = roleRepository;
     }
 
-    @Override
     public Role save(Role role) {
         return roleRepository.save(role);
     }
 
-    @Override
     public void delete(Role role) {
         roleRepository.delete(role);
 
     }
 
-    @Override
     public void delete(Long id) {
         roleRepository.deleteById(id);
 
     }
 
-    @Override
     public Role update(Role role) {
         return roleRepository.save(role);
 
     }
 
-    @Override
     public Role findById(Long id) {
         return roleRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("Post not Found"));

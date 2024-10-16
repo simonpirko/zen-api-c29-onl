73c069b4db8b4b8f730686e246f4f081283f0786
Yakubchyk <ivan.yakubchyk@gmail.com>
2024-10-16 15:37:31 +0200

    13. Исправления Category, Comment, DisLike, LikeService.

--- src/main/java/by/tms/zenapic29onl/service/LikeService.java
+++ src/main/java/by/tms/zenapic29onl/service/LikeService.java
@@ -6,34 +6,29 @@ import by.tms.zenapic29onl.repository.LikeRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class LikeService implements CrudOperation<Like> {
+public class LikeService {
     private final LikeRepository likeRepository;
 
     public LikeService(LikeRepository likeRepository) {
         this.likeRepository = likeRepository;
     }
 
-    @Override
     public Like save(Like like) {
         return likeRepository.save(like);
     }
 
-    @Override
     public void delete(Like like) {
         likeRepository.delete(like);
     }
 
-    @Override
     public void delete(Long id) {
         likeRepository.deleteById(id);
     }
 
-    @Override
     public Like update(Like like) {
         return likeRepository.save(like);
     }
 
-    @Override
     public Like findById(Long id) {
         return likeRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("Like not Found"));
